ce01250ff99699ae28000940d0cc77b7bec7a437
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2026-02-25 13:03:10 +0100

    move handling fn outside Effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,16 @@ import { useEffect, useState } from "react";
 export const NavBar = () => {
   const [scrolled, setScrolled] = useState(false);
 
+  const handleScroll = () => {
+    if (window.scrollY > 10) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 10) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
     window.addEventListener("scroll", handleScroll);
-
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
