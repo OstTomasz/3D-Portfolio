d46f5096922d6cc1a4bba1c5323c28858341541b
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2026-04-15 10:41:06 +0200

    Refactor Showcase component and CSS for improved layout and responsiveness. Adjust flex properties and spacing for project elements, ensuring consistent styling across different screen sizes.

--- src/sections/Showcase.tsx
+++ src/sections/Showcase.tsx
@@ -2,6 +2,7 @@ import { GitHubButton } from "@/components/GitHubButton";
 import { GITHUB_URL, projects } from "@/constants/constants";
 import { useShowcaseAnimations } from "@/hooks/useShowcaseAnimations";
 
+// Showcase.tsx
 export const Showcase = () => {
   const {
     sectionRef,
@@ -16,11 +17,11 @@ export const Showcase = () => {
 
   return (
     <section id="work" ref={sectionRef} className="app-showcase">
-      <div className="w-full">
+      <div className="w-full flex flex-col">
         <div className="showcaselayout">
-          {/* Left - Main Project */}
+          {/* Left */}
           <div className="first-project-col">
-            <div className="relative ">
+            <div className="relative flex flex-col flex-1">
               <GitHubButton href={project1.githubLink} />
               <a
                 className="first-project-wrapper transition-transform duration-300 hover:scale-[1.02] hover:-translate-y-1 hover:brightness-105"
@@ -43,7 +44,6 @@ export const Showcase = () => {
               </a>
             </div>
 
-            {/* GitHub Link - desktop only */}
             <a
               href={GITHUB_URL}
               target="_blank"
@@ -52,7 +52,7 @@ export const Showcase = () => {
             >
               <div
                 ref={githubDesktopRef}
-                className="border-2 rounded-2xl bg-black-200 border-white-50 p-3 flex-col-center"
+                className="border-2 rounded-2xl bg-black-200 border-white-50 p-5 flex-col-center h-full"
               >
                 <div className="mb-4">
                   <img src="/images/github-logo.png" alt="GitHub" />
@@ -64,64 +64,69 @@ export const Showcase = () => {
             </a>
           </div>
 
-          {/* Right - Project List */}
+          {/* Right */}
           <div className="project-list-wrapper">
-            <div className="relative">
+            <div className="relative flex-1">
               <GitHubButton href={project2.githubLink} />
               <a
                 href={project2.liveLink}
                 target="_blank"
                 rel="noreferrer"
-                className=" transition-transform duration-300 hover:scale-[1.02] hover:-translate-y-1 hover:brightness-105 block"
+                className="transition-transform duration-300 hover:scale-[1.02] hover:-translate-y-1 hover:brightness-105 block h-full"
               >
                 <div
                   ref={project2Ref}
-                  className="relative project border-2 rounded-2xl bg-black-200 border-white-50 p-3 flex flex-col"
+                  className="relative project border-2 rounded-2xl bg-black-200 border-white-50 p-3 flex flex-col items-center justify-center h-full"
                 >
                   <div className="image-wrapper">
                     <img src={project2.imgPath} alt={project2.title} />
                   </div>
-                  <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mt-5">
-                    {project2.title}
-                  </h2>
-                  <p className="text-white-50 md:text-xl mt-2">
-                    {project2.desc}
-                  </p>
+                  <div className="text-content">
+                    <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mt-5">
+                      {project2.title}
+                    </h2>
+                    <p className="text-white-50 md:text-xl mt-2">
+                      {project2.desc}
+                    </p>
+                  </div>
                 </div>
               </a>
             </div>
-            <div className="relative">
+
+            <div className="relative flex-1">
               <GitHubButton href={project3.githubLink} />
               <a
                 href={project3.liveLink}
                 target="_blank"
                 rel="noreferrer"
-                className="relative transition-transform duration-300 hover:scale-[1.02] hover:-translate-y-1 hover:brightness-105 block"
+                className="transition-transform duration-300 hover:scale-[1.02] hover:-translate-y-1 hover:brightness-105 block h-full"
               >
                 <div
                   ref={project3Ref}
-                  className="relative project border-2 rounded-2xl bg-black-200 border-white-50 p-3 flex flex-col"
+                  className="relative project border-2 rounded-2xl bg-black-200 border-white-50 p-3 flex flex-col h-full items-center justify-center"
                 >
                   <div className="image-wrapper">
                     <img src={project3.imgPath} alt={project3.title} />
                   </div>
-                  <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mt-5">
-                    {project3.title}
-                  </h2>
-                  <p className="text-white-50 md:text-xl mt-2">
-                    {project3.desc}
-                  </p>
+                  <div className="text-content">
+                    <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mt-5">
+                      {project3.title}
+                    </h2>
+                    <p className="text-white-50 md:text-xl mt-2">
+                      {project3.desc}
+                    </p>
+                  </div>
                 </div>
               </a>
             </div>
           </div>
 
-          {/* GitHub Link - mobile/tablet */}
+          {/* GitHub mobile */}
           <a
             href={GITHUB_URL}
             target="_blank"
             rel="noreferrer"
-            className="github-link-wrapper block 2xl:hidden w-full transition-transform duration-300 hover:scale-[1.02] hover:-translate-y-1 hover:brightness-105"
+            className="github-link-wrapper  block 2xl:hidden w-full transition-transform duration-300 hover:scale-[1.02] hover:-translate-y-1 hover:brightness-105"
           >
             <div
               ref={githubMobileRef}
